26b405e8b2e89f0e1d28919cdc7c473df559e447
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2022-12-03 13:46:00 +0100

    Update user.entity.ts

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,52 +1,57 @@
-import {Column, Entity, Generated, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn} from "typeorm";
-import {type} from "os";
-import {Sola} from "./sole.entity";
-import {Razred} from "./razredi.entity";
-import {Ceuviz} from "./ceuviz.entity";
-
+import {
+  Column,
+  Entity,
+  Generated,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { type } from 'os';
+import { Sola } from './sole.entity';
+import { Razred } from './razredi.entity';
+import { Ceuviz } from './ceuviz.entity';
 
 @Entity('Dijaki')
 export class User {
-    @PrimaryGeneratedColumn('increment')
-    id:number;
+  @PrimaryGeneratedColumn('increment')
+  id: number;
 
-    @Column()
-    ime:string;
+  @Column()
+  ime: string;
 
-    @Column()
-    priimek:string;
+  @Column()
+  priimek: string;
 
-    @Column({unique:true})
-    eposta:string;
+  @Column({ unique: true })
+  eposta: string;
 
-    @Column()
-    geslo:string;
+  @Column()
+  geslo: string;
 
-    @Column()
-    telefon:string;
+  @Column()
+  telefon: string;
 
-    @Column()
-    emso:number;
+  @Column()
+  emso: number;
 
-    @Column()
-    davcna:number;
+  @Column()
+  davcna: number;
 
-    @Column('date')
-    datumroj:Date;
+  @Column('date')
+  datumroj: Date;
 
-    @Column('real' , {default: 0.0})
-    stanjerac:number;
+  @Column('real', { default: 0.0 })
+  stanjerac: number;
 
-    @Column( {default: false})
-    isadmin:boolean;
+  @Column({ default: false })
+  isadmin: boolean;
 
-    @ManyToOne(type => Razred, razred => razred.id)
-    @JoinColumn({name: 'razred_id'})
-    razred:Razred;
+  @ManyToOne((type) => Razred, (razred) => razred.id)
+  @JoinColumn({ name: 'razred_id' })
+  razred: Razred;
 
-    @ManyToOne(type => Ceuviz, ceuviz => ceuviz.id)
-    @JoinColumn({name: 'ceuviz_id'})
-    ceuviz:Ceuviz;
-
-
+  @ManyToOne((type) => Ceuviz, (ceuviz) => ceuviz.id)
+  @JoinColumn({ name: 'ceuviz_id' })
+  ceuviz: Ceuviz;
 }
